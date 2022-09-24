68ae3cc2e3564041964dd6f67277c8d9dd3f516e
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-23 23:38:09 -0300

    Manejo de dom y ediciones con css

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let computadoraPuntaje = 0;
-let jugadorPuntaje = 0;
+const buttons = document.querySelectorAll('button');
+const para = document.querySelector('p');
 
 function getComputerChoice() {
     let eleccion = Math.floor(Math.random() * 3) + 1;
@@ -9,78 +9,38 @@ function getComputerChoice() {
     } else if (eleccion == 2) {
         return 'Papel';
     } else if (eleccion == 3) {
-        return 'Tijera';
+        return 'Tijeras';
     }
 } 
 
-function getPlayerChoice() {
-    let eleccion = prompt('Elija un numero del 1 al 3 (1. Piedra | 2. Papel | 3. Tijera)');
-
-    if (eleccion == 1) {
-        return 'Piedra';
-    } else if (eleccion == 2) {
-        return 'Papel';
-    } else if (eleccion == 3) {
-        return 'Tijera';
-    } else {
-        return eleccion;
-    }
-}
-
-function playRound(getComputerChoice, getPlayerChoice) {
+function playRound(getComputerChoice, eleccion) {
     let computadora = getComputerChoice();
-    let jugador = getPlayerChoice();
+    let jugador = eleccion;
 
-    if (computadora == 'Piedra' && jugador == 'Piedra') {
-        alert('Empate! Ambos eligieron Piedra');
+    if (computadora == jugador) {
+        para.textContent = `Empate! Ambos eligieron ${jugador}`;
     } else if (computadora == 'Piedra' && jugador == 'Papel') {
-        alert('El jugador gana! El Papel puede por sobre la Piedra');
-        jugadorPuntaje += 1;
-    } else if (computadora == 'Piedra' && jugador == 'Tijera') { 
-        alert('Gana la maquina! Piedra contra Tijera');
-        computadoraPuntaje += 1;
-    }
-
-    else if (computadora == 'Papel' && jugador == 'Piedra') {
-        alert('La computadora gana! Papel sobre Piedra');
-        computadoraPuntaje += 1;
-    } else if (computadora == 'Papel' && jugador == 'Papel') {
-        alert('Empate! Ambos tuvieron el valor de elegir Papel');
-    } else if (computadora == 'Papel' && jugador == 'Tijera') { 
-        alert('El jugador gana! Tijera sobre Papel');
-        jugadorPuntaje += 1;
-    }
-
-    else if (computadora == 'Tijera' && jugador == 'Piedra') {
-        alert('El jugador gana! Con una bella eleccion de Piedra')
-        jugadorPuntaje += 1;
-    } else if (computadora == 'Tijera' && jugador == 'Papel') {
-        alert('La computadora gana! Tijera puede a Papel');
-        computadoraPuntaje += 1;
-    } else if (computadora == 'Tijera' && jugador == 'Tijera') { 
-        alert('Empate! Dos tijeras, pongale onda');
-    } 
-    
-    else {
+        para.textContent = `El jugador gana! El Papel puede por sobre la Piedra`;
+    } else if (computadora == 'Piedra' && jugador == 'Tijeras') { 
+        para.textContent = `Gana la maquina! Piedra contra Tijeras`;
+    } else if (computadora == 'Papel' && jugador == 'Piedra') {
+        para.textContent = `La computadora gana! Papel sobre Piedra`;
+    } else if (computadora == 'Papel' && jugador == 'Tijeras') { 
+        para.textContent = `El jugador gana! Tijeras sobre Papel`;
+    } else if (computadora == 'Tijeras' && jugador == 'Piedra') {
+        para.textContent = `El jugador gana! Con una bella eleccion de Piedra`;
+    } else if (computadora == 'Tijeras' && jugador == 'Papel') {
+        para.textContent = `La computadora gana! Tijera puede a Papel`;
+    } else {
         alert(`Algo salio mal, reintente. Fijese si su eleccion (${jugador}) es correspondiente a las opciones validas.`);
     }
 }
 
-function play5Rounds(playRound) {
-    for (let i = 0; i < 5; i++) {
-        playRound(getComputerChoice, getPlayerChoice);
-    }
-
-    if (jugadorPuntaje > computadoraPuntaje) {
-        alert(`El jugador gana con una puntacion de ${jugadorPuntaje} dejando atras a la maquina que se lleva ${computadoraPuntaje} punto/s. Felicitaciones!`);
-    } else if (computadoraPuntaje > jugadorPuntaje) {
-        alert(`Lamentablemente la computadora ha ganado con una puntacion de ${computadoraPuntaje} contra tu/s ${jugadorPuntaje} punto/s. Mucha suerte para la proxima!`);
-    } else {
-        alert(`Oh no! Hemos recibido un empate de ${jugadorPuntaje} punto/s de tu parte y ${computadoraPuntaje} punto/s de tu rival. No odias cuando eso pasa?`);
-    }
-}
-
-play5Rounds(playRound);
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(getComputerChoice, button.name);
+    })
+})
 
 
 
